84d161b8373033e92b093bec2cdcc0a08df7e57d
tatamiya <tamyuji1002@gmail.com>
2021-12-25 18:52:32 +0900

    8.1.5A flash test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       # Log in
     else
       # Create an error message
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
