e9fc035c3304779ff4bcba01339cc4046f563b34
tatamiya <tamyuji1002@gmail.com>
2021-12-26 11:44:18 +0900

    8.2.1The log_in method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log in
+      reset_session
+      log_in user
+      redirect_to user
     else
       # Create an error message
       flash.now[:danger] = 'Invalid email/password combination'
