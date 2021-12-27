32fa1782d8bf3680dcdd0cfd520295d1a83eca34
tatamiya <tamyuji1002@gmail.com>
2021-12-27 22:21:14 +0900

    8.2.3Changing the layout links

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,4 +9,9 @@ module SessionsHelper
             @curretn_user ||= User.find_by(id: session[:user_id])
         end
     end
+
+    # Returns true if the user is logged in, false otherwise.
+    def logged_in?
+        !current_user.nil?
+    end
 end
