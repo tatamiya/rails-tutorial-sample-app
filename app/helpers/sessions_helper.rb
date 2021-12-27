625138f16a3006a6302414b59ca453cb11c47fb0
tatamiya <tamyuji1002@gmail.com>
2021-12-27 22:21:32 +0900

    fix a typo

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ module SessionsHelper
     # Returns the current logged-in user (if any)
     def current_user
         if session[:user_id]
-            @curretn_user ||= User.find_by(id: session[:user_id])
+            @current_user ||= User.find_by(id: session[:user_id])
         end
     end
 
