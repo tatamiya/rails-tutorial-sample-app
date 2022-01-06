bb32c56f23dcfa668b6b093bff2aef8a24dc20e6
tatamiya <tamyuji1002@gmail.com>
2022-01-06 23:00:08 +0900

    9.3.2Testing the remember branch

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,9 @@
 module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
+
+        # Guard against session replay attacks.
+        session[:session_token] = user.session_token
     end
 
     # Remembers a user in a persistent session.
@@ -13,7 +16,10 @@ module SessionsHelper
     # Returns the user corresponding to the remember token cookie.
     def current_user
         if (user_id = session[:user_id])
-            @current_user ||= User.find_by(id: user_id)
+            user = User.find_by(id: user_id)
+            if user && session[:session_token] == user.session_token
+                @current_user = user
+            end
         elsif (user_id = cookies.encrypted[:user_id])
             user = User.find_by(id: user_id)
             if user && user.authenticated?(cookies[:remember_token])
