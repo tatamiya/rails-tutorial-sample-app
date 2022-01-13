3d9aff3a032ddf874b2f7a7727065cf3272ad86c
tatamiya <tamyuji1002@gmail.com>
2022-01-13 22:12:10 +0900

    10.3.1Users index (Exercise, layout test before login)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,11 +2,12 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  test "layout links before login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", login_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
 
