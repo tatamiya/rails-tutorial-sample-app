7f644c36398910fba552d24a6401c6a8e311b087
tatamiya <tamyuji1002@gmail.com>
2022-01-16 14:43:45 +0900

    10.4.1Administrative users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_31_030710) do
+ActiveRecord::Schema.define(version: 2022_01_16_053828) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_12_31_030710) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
