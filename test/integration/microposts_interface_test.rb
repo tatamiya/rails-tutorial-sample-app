1b99e23329944e6b063a131d467902a2d930aa65
tatamiya <tamyuji1002@gmail.com>
2022-02-12 11:12:58 +0900

    13.3.5Micropost tests (Integration test, invalid submission)

---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "micropost interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+
+    # Invalid submission
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+    assert_select 'a[href=?]', '/?page=2'
+
+    # Valid submission
+
+  end
+  # test "the truth" do
+  #   assert true
+  # end
+end
