fe615ba247f6a6a5652d4d284785c01f95eaa53a
tatamiya <tamyuji1002@gmail.com>
2022-02-15 21:54:44 +0900

    13.4.1Basic image upload (Exercise 2)

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     # Invalid submission
     assert_no_difference 'Micropost.count' do
@@ -20,9 +21,12 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid submission
     content = "This micropost really ties the room together"
+    image = fixture_file_upload('test/fixtures/kitten.jpg', 'image/jpeg')
     assert_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: content }}
+      post microposts_path, params: { micropost:
+                                    { content: content, image: image }}
     end
+    assert assigns(:micropost).image.attached?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
